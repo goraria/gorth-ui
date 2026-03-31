eb54f49e0d4b341df662f08cf02361a5e91d78a0
goraria <gorariagroup@gmail.com>
2026-04-01 02:53:22 +0700

    fix

--- src/components/custom/sidebar.tsx
+++ src/components/custom/sidebar.tsx
@@ -666,7 +666,11 @@ function SidebarMenuSub({ className, ...props }: React.ComponentProps<"ul">) {
       data-slot="sidebar-menu-sub"
       data-sidebar="menu-sub"
       className={cn(
-        "mx-3.5 flex min-w-0 translate-x-px flex-col gap-1 border-l border-sidebar-border px-2.5 py-0.5 group-data-[collapsible=icon]:hidden",
+        // "mx-3.5 flex min-w-0 translate-x-px flex-col gap-1 border-l border-sidebar-border px-2.5 py-0.5 group-data-[collapsible=icon]:hidden",
+        // "border-sidebar-border mx-3.5 flex min-w-0 translate-x-px flex-col gap-1 border-l px-2.5 py-0.5",
+        "flex min-w-0 translate-x-px flex-col gap-1 pt-1",
+        // "mx-3.5 flex min-w-0 flex-col gap-1 pl-5 py-0.5",
+        "group-data-[collapsible=icon]:hidden",
         className
       )}
       {...props}
