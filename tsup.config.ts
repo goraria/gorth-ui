86aa3a65da3f794422c701a0c2509e5f7f1c0912
goraria <gorariagroup@gmail.com>
2026-04-01 23:21:36 +0700

    fix

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,27 @@
+/// <reference types="node" />
+
+import { copyFileSync, mkdirSync } from "node:fs"
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
+  clean: true,
   dts: true,
-  splitting: false,
-  clean: true
+  entry: [
+    "src/index.ts",
+    "src/components/*/*.tsx",
+    "src/hooks/*.ts",
+    "src/hooks/*.tsx",
+    "src/lib/*.ts",
+    "src/cores/*.ts",
+  ],
+  format: ["esm", "cjs"],
+  sourcemap: false,
+  minify: true,
+  target: "es2024",
+  outDir: "dist",
+  treeshake: true,
+  onSuccess: async () => {
+    mkdirSync("dist", { recursive: true })
+    copyFileSync("src/styles/globals.css", "dist/globals.css")
+  },
 })
